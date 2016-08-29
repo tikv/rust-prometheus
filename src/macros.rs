68190a565608c6a1e92429ab68b6f00901830687
overvenus <overvenus@gmail.com>
2016-08-29 14:59:50 +0800

    Address comments

--- src/macros.rs
+++ src/macros.rs
@@ -13,51 +13,142 @@
 
 #[macro_export]
 macro_rules! labels {
-    ($($KEY: expr => $VALUE: expr ,)+) => {
+    ($($KEY: expr => $VALUE: expr ,)*) => {
         {
             use std::collections::HashMap;
 
-            let mut lbs = HashMap::<String, String>::new();
+            let lbs = HashMap::new();
             $(
-                lbs.insert(($KEY).into(), ($VALUE).into());
+                let mut lbs = lbs;
+                lbs.insert($KEY, $VALUE);
             )*
 
             lbs
         }
-    };
-
-    () => {
-        HashMap::<String, String>::new()
     }
 }
 
 #[macro_export]
 macro_rules! opts {
-    ($NAME: expr, $HELP: expr, $LABELS: expr) => {
-        $crate::Opts::new($NAME, $HELP).const_labels($LABELS)
+    ($NAME: expr, $HELP: expr) => {
+        $crate::Opts::new($NAME, $HELP)
+    };
+
+    ($NAME: expr, $HELP: expr $(, $LABELS: expr)+) => {
+        {
+            use std::collections::HashMap;
+
+            let opts = opts!($NAME, $HELP);
+            let mut lbs = HashMap::<String, String>::new();
+            $(
+                lbs.extend($LABELS.iter().map(|(k, v)| ((*k).into(), (*v).into())));
+            )+
+
+            opts.const_labels(lbs)
+        }
     }
 }
 
 #[macro_export]
 macro_rules! register_counter_with {
+    ($NAME: expr, $HELP: expr $(, $LABELS: expr)*) => {
+        register_counter_with!(opts!($NAME, $HELP $(, $LABELS)*))
+    };
+
     ($OPTS: expr) => {
         {
             let counter = $crate::Counter::with_opts($OPTS).unwrap();
-            $crate::register(Box::new(counter.clone())).unwrap();
-
-            counter
+            $crate::register(Box::new(counter.clone())).map(|_| counter)
         }
     }
 }
 
 #[macro_export]
 macro_rules! register_gauge_with {
+    ($NAME: expr, $HELP: expr $(, $LABELS: expr)*) => {
+        register_gauge_with!(opts!($NAME, $HELP $(, $LABELS)*))
+    };
+
     ($OPTS: expr) => {
         {
             let gauge = $crate::Gauge::with_opts($OPTS).unwrap();
-            $crate::register(Box::new(gauge.clone())).unwrap();
-
-            gauge
+            $crate::register(Box::new(gauge.clone())).map(|_| gauge)
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_macro_labels() {
+        let labels = labels!{
+            "test" => "hello",
+            "foo" => "bar",
+        };
+        assert_eq!(labels.len(), 2);
+        assert!(labels.get("test").is_some());
+        assert_eq!(*(labels.get("test").unwrap()), "hello");
+
+        let labels: HashMap<&str, &str> = labels!{};
+        assert!(labels.is_empty());
+    }
+
+    #[test]
+    fn test_macro_opts() {
+        let name = "test_opts";
+        let help = "test opts help";
+
+        let opts = opts!(name, help);
+
+        assert_eq!(opts.name, name);
+        assert_eq!(opts.help, help);
+
+        let opts = opts!(name, help, labels!{"test" => "hello", "foo" => "bar",});
+        assert_eq!(opts.const_labels.len(), 2);
+        assert!(opts.const_labels.get("foo").is_some());
+        assert_eq!(opts.const_labels.get("foo").unwrap(), "bar");
+
+        let opts = opts!(name,
+                         help,
+                         labels!{"test" => "hello", "foo" => "bar",},
+                         labels!{"ans" => "42",});
+        assert_eq!(opts.const_labels.len(), 3);
+        assert!(opts.const_labels.get("ans").is_some());
+        assert_eq!(opts.const_labels.get("ans").unwrap(), "42");
+    }
+
+    #[test]
+    fn test_macro_counter() {
+        let opts = opts!("test_macro_counter_1",
+                         "help",
+                         labels!{"test" => "hello", "foo" => "bar",});
+
+        let res1 = register_counter_with!(opts);
+        assert!(res1.is_ok());
+
+        let res2 = register_counter_with!("test_macro_counter_2", "help");
+        assert!(res2.is_ok());
+
+        let res3 = register_counter_with!("test_macro_counter_3", "help", labels!{ "a" => "b",});
+        assert!(res3.is_ok());
+    }
+
+    #[test]
+    fn test_macro_gauge() {
+        let opts = opts!("test_macro_gauge",
+                         "help",
+                         labels!{"test" => "hello", "foo" => "bar",});
+
+        let res1 = register_gauge_with!(opts);
+        assert!(res1.is_ok());
+
+        let res2 = register_gauge_with!("test_macro_gauge_2", "help");
+        assert!(res2.is_ok());
+
+        let res3 = register_gauge_with!("test_macro_gauge_3", "help", labels!{"a" => "b",});
+        assert!(res3.is_ok());
     }
 }
