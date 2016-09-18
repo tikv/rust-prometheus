36a0b669a9061761cfeeb2bb5d2ebd3f66eb0d01
overvenus <overvenus@gmail.com>
2016-09-18 13:11:52 +0800

    Address comments

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![cfg_attr(all(test, feature="dev"), deny(warnings))]
 #![cfg_attr(feature="dev", feature(plugin))]
 #![cfg_attr(feature="dev", plugin(clippy))]
 
