3bd498c7695958b5cce304b117f71f6665b6c5b2
overvenus <overvenus@gmail.com>
2016-09-25 21:12:33 +0800

    add a test

--- src/vec.rs
+++ src/vec.rs
@@ -298,7 +298,7 @@ mod tests {
 
     use counter::CounterVec;
     use gauge::GaugeVec;
-    use metrics::Opts;
+    use metrics::{Opts, Metric};
 
     #[test]
     fn test_counter_vec_with_labels() {
@@ -383,5 +383,25 @@ mod tests {
 
         assert!(vec.remove_label_values(&["v1"]).is_err());
         assert!(vec.remove_label_values(&["v1", "v3"]).is_err());
+    }
+
+    #[test]
+    fn test_vec_get_metric_with() {
+        let vec = CounterVec::new(Opts::new("test_vec", "test counter vec help"),
+                                  &["b", "c", "a"])
+            .unwrap();
+
+        // create a new metric that labels are {b" => "c", "c" => "a" "a" => "b"}.
+        let mut labels = HashMap::new();
+        labels.insert("a", "b");
+        labels.insert("b", "c");
+        labels.insert("c", "a");
+        let c = vec.get_metric_with(&labels).unwrap();
+        let m = c.metric();
+        let label_pairs = m.get_label();
+        assert_eq!(label_pairs.len(), labels.len());
+        for lp in label_pairs.iter() {
+            assert_eq!(lp.get_value(), *labels.get(&lp.get_name()).unwrap());
+        }
     }
 }
