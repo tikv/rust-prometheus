dd3d792a42fe3df8c2aa6838160d77f764a8c732
Neil Shen <overvenus@gmail.com>
2017-03-13 10:55:54 +0800

    fix unused-imports

--- examples/example_process_collector.rs
+++ examples/example_process_collector.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[macro_use]
 extern crate prometheus;
 
 #[cfg(all(feature = "process", target_os="linux"))]
