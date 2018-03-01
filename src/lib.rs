44956a62bbb0f96ca1049144485503f11166b15d
Breezewish <me@breeswish.org>
2018-03-01 17:26:55 +0800

    grumble

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(test)]
+#![cfg_attr(test, feature(test))]
 #![cfg_attr(feature = "dev", feature(plugin))]
 #![cfg_attr(feature = "dev", plugin(clippy))]
 #![cfg_attr(not(feature = "dev"), allow(unknown_lints))]
