8a4592fda70f757017d6db2f330dc4f6c156c0c8
Breezewish <me@breeswish.org>
2018-04-17 23:04:23 +0800

    add deep nested static metrics benchmark

--- static-metric/benches/benches.rs
+++ static-metric/benches/benches.rs
@@ -88,3 +88,42 @@ fn bench_static_metrics_macro_with_lookup(b: &mut Bencher) {
     let static_counter = StaticCounter2::from(&counter_vec);
     b.iter(|| static_counter.get("foo").get("bar").inc());
 }
+
+make_static_metric! {
+    pub struct StaticCounter3: IntCounter {
+        "d1" => { val1 },
+        "d2" => { val2 },
+        "d3" => { val3 },
+        "d4" => { val4 },
+        "d5" => { val5 },
+        "d6" => { val6 },
+        "d7" => { val7 },
+        "d8" => { val8 },
+        "d9" => { val9 },
+        "d10" => { val10 },
+    }
+}
+
+#[bench]
+/// macro implemented static metrics performance, with a deep nesting level.
+fn bench_static_metrics_macro_deep(b: &mut Bencher) {
+    let counter_vec = IntCounterVec::new(
+        Opts::new("foo", "bar"),
+        &["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10"],
+    ).unwrap();
+    let static_counter = StaticCounter3::from(&counter_vec);
+    b.iter(|| {
+        static_counter
+            .val1
+            .val2
+            .val3
+            .val4
+            .val5
+            .val6
+            .val7
+            .val8
+            .val9
+            .val10
+            .inc()
+    });
+}
