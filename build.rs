b91c711fa9b76abc910f56b0ca1c612b53c1524d
Flier Lu <flier.lu@gmail.com>
2018-05-14 11:30:24 +0800

    auto generate protobuf binding file when set `gen` feature

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
 extern crate protoc_rust;
 
 fn main() {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "proto",
-        input: &["proto/metrics.proto"],
-        includes: &["proto"],
-        customize: protoc_rust::Customize {
-            ..Default::default()
-        },
-    }).expect("protoc");
+    if cfg!(feature = "gen") {
+        protoc_rust::run(protoc_rust::Args {
+            out_dir: "proto",
+            input: &["proto/metrics.proto"],
+            includes: &["proto"],
+            customize: protoc_rust::Customize {
+                ..Default::default()
+            },
+        }).expect("protoc");
+    }
 }
