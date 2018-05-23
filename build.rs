8724e5af587f49eb69d51a97e9da62da5110c953
Flier Lu <flier@users.noreply.github.com>
2018-05-23 10:33:13 +0800

    upgrade protobuf to v2.0 and gen binding file in the build script (#171)

---
+++ build.rs
@@ -0,0 +1,19 @@
+#[cfg(feature = "gen")]
+extern crate protobuf_codegen_pure;
+
+#[cfg(feature = "gen")]
+fn generate_protobuf_binding_file() {
+    protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
+        out_dir: "proto",
+        input: &["proto/metrics.proto"],
+        includes: &["proto"],
+        ..Default::default()
+    }).unwrap();
+}
+
+#[cfg(not(feature = "gen"))]
+fn generate_protobuf_binding_file() {}
+
+fn main() {
+    generate_protobuf_binding_file()
+}
