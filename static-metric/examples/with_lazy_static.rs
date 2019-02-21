ad98e5b92aa8e65d48ac033436971045db295717
Brian Anderson <andersrb@gmail.com>
2019-02-21 00:23:06 -0800

    Update feature attributes

--- static-metric/examples/with_lazy_static.rs
+++ static-metric/examples/with_lazy_static.rs
@@ -11,8 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(proc_macro)]
-
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
