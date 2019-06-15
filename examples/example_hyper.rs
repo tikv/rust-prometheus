91fbd1d83ec217bb9f2140200bcb2475a27081da
crash <crash@inventati.org>
2019-06-15 18:11:05 +0200

    Update hyper example to work with hyper 0.12

--- examples/example_hyper.rs
+++ examples/example_hyper.rs
@@ -16,9 +16,12 @@ extern crate lazy_static;
 #[macro_use]
 extern crate prometheus;
 
-use hyper::header::ContentType;
-use hyper::mime::Mime;
-use hyper::server::{Request, Response, Server};
+use hyper::{
+    Body, Response, Server,
+    header::CONTENT_TYPE,
+    rt::Future,
+    service::service_fn_ok,
+};
 
 use prometheus::{Counter, Encoder, Gauge, HistogramVec, TextEncoder};
 
@@ -44,24 +47,35 @@ lazy_static! {
 }
 
 fn main() {
-    let encoder = TextEncoder::new();
-    let addr = "127.0.0.1:9898";
-    println!("listening addr {:?}", addr);
-    Server::http(addr)
-        .unwrap()
-        .handle(move |_: Request<'_, '_>, mut res: Response<'_>| {
+    let addr = ([127, 0, 0, 1], 9898).into();
+    println!("Listening address: {:?}", addr);
+
+    let new_service = || {
+        let encoder = TextEncoder::new();
+        service_fn_ok(move |_request| {
             HTTP_COUNTER.inc();
             let timer = HTTP_REQ_HISTOGRAM.with_label_values(&["all"]).start_timer();
 
             let metric_families = prometheus::gather();
             let mut buffer = vec![];
             encoder.encode(&metric_families, &mut buffer).unwrap();
-            res.headers_mut()
-                .set(ContentType(encoder.format_type().parse::<Mime>().unwrap()));
-            res.send(&buffer).unwrap();
+            HTTP_BODY_GAUGE.set(buffer.len() as f64);
+
+            let response = Response::builder()
+                .status(200)
+                .header(CONTENT_TYPE, encoder.format_type())
+                .body(Body::from(buffer))
+                .unwrap();
 
             timer.observe_duration();
-            HTTP_BODY_GAUGE.set(buffer.len() as f64);
+
+            response
         })
-        .unwrap();
+    };
+
+    let server = Server::bind(&addr)
+        .serve(new_service)
+        .map_err(|e| eprintln!("Server error: {}", e));
+
+    hyper::rt::run(server);
 }
