d9e44a1e49826832a91a41f8d1f921d7f2c32de8
koushiro <koushiro.cqx@gmail.com>
2019-10-15 10:27:14 +0800

    Use thiserror instead of failure
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -11,44 +11,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use failure::Fail;
+use thiserror::Error;
+
+/// The error types for prometheus.
+#[derive(Debug, Error)]
+pub enum Error {
+    /// A duplicate metric collector has already been registered.
+    #[error("Duplicate metrics collector registration attempted")]
+    AlreadyReg,
+    /// The label cardinality was inconsistent.
+    #[error("Inconsistent label cardinality, expect {0} label values, but got {1}")]
+    InconsistentCardinality(usize, usize),
+    /// An error message which is only a string.
+    #[error("Error: {0}")]
+    Msg(String),
+    /// An error containing a [`std::io::Error`].
+    #[error("Io error: {0}")]
+    Io(#[from] std::io::Error),
+    /// An error containing a [`protobuf::Error`].
+    #[cfg(feature = "protobuf")]
+    #[error("Protobuf error: {0}")]
+    Protobuf(#[from] protobuf::error::ProtobufError),
+}
 
 /// A specialized Result type for prometheus.
 pub type Result<T> = std::result::Result<T, Error>;
-
-/// The error types for prometheus.
-#[derive(Debug, Fail)]
-pub enum Error {
-    /// A duplicate metric collector has already been registered.
-    #[fail(display = "Duplicate metrics collector registration attempted")]
-    AlreadyReg,
-    /// The label cardinality was inconsistent.
-    #[fail(
-        display = "Inconsistent label cardinality, expect {} label values, but got {}",
-        _0, _1
-    )]
-    InconsistentCardinality(usize, usize),
-    /// An error message which is only a string.
-    #[fail(display = "Error: {}", _0)]
-    Msg(String),
-    /// An error containing a [`std::io::Error`].
-    #[fail(display = "Io error: {}", _0)]
-    Io(std::io::Error),
-    /// An error containing a [`protobuf::Error`].
-    #[cfg(feature = "protobuf")]
-    #[fail(display = "Protobuf error: {}", _0)]
-    Protobuf(protobuf::error::ProtobufError),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
-        Error::Io(err)
-    }
-}
-
-#[cfg(feature = "protobuf")]
-impl From<protobuf::error::ProtobufError> for Error {
-    fn from(err: protobuf::error::ProtobufError) -> Self {
-        Error::Protobuf(err)
-    }
-}
