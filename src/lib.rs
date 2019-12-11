d70cfe14f05db300f89457273f7ead03d073deb3
Renkai <gaelookair@gmail.com>
2019-12-11 14:57:07 +0800

    add LocalMetric implementation of make_static_metric! pass cargo check
    Signed-off-by: Renkai <gaelookair@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -158,7 +158,6 @@ pub mod local {
     Unsync local metrics, provides better performance.
 
     */
-    
     pub use super::counter::{LocalCounter, LocalCounterVec, LocalIntCounter, LocalIntCounterVec};
     pub use super::histogram::{LocalHistogram, LocalHistogramTimer, LocalHistogramVec};
     pub use super::metrics::LocalMetric;
