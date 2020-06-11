6dffad528b4d538fe2af5e00978cc358b5a954a3
Max Inden <mail@max-inden.de>
2020-06-11 15:31:56 +0200

    src/histogram: Fix fmt complaints
    Signed-off-by: Max Inden <mail@max-inden.de>

--- src/histogram.rs
+++ src/histogram.rs
@@ -1235,7 +1235,7 @@ mod tests {
                 "Used as an example",
                 vec![0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 5.0]
             )
-                .variable_label("example_variable"),
+            .variable_label("example_variable"),
         );
 
         if let Err(Error::InconsistentCardinality { expect, got }) = hist {
