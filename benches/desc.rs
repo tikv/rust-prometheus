a12f22cd81bb4875118b8f717e34aaa71d031dad
Luca BRUNO <luca.bruno@coreos.com>
2020-11-06 15:46:40 +0000

    benches: move to criterion-rs and GitHub actions
    This moves all benches to criterion-rs, in order to allow running
    benchmarks on a stable toolchain.
    This also moves benchmarking and linting jobs to a pinned toolchain,
    under GitHub actions.
    
    Signed-off-by: Luca BRUNO <luca.bruno@coreos.com>

--- benches/desc.rs
+++ benches/desc.rs
@@ -11,22 +11,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use prometheus::core::Desc;
 
-use test::{black_box, Bencher};
-
-#[bench]
-fn description_validation(b: &mut Bencher) {
-    b.iter(|| {
-        black_box(Desc::new(
-            "api_http_requests_total".to_string(),
-            "not empty help".to_string(),
-            vec!["method".to_string(), "handler".to_string()],
-            Default::default(),
-        ))
+fn description_validation(c: &mut Criterion) {
+    c.bench_function("description_validation", |b| {
+        b.iter(|| {
+            black_box(Desc::new(
+                "api_http_requests_total".to_string(),
+                "not empty help".to_string(),
+                vec!["method".to_string(), "handler".to_string()],
+                Default::default(),
+            ))
+        });
     });
 }
+
+criterion_group!(benches, description_validation);
+criterion_main!(benches);
