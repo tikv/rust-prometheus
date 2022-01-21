df4e1f2967470045545822acee8f50a70f461aaf
Han Xu <keepsimple@gmail.com>
2022-01-21 09:43:48 -0800

    fix doc for encode
    Signed-off-by: Han Xu <keepsimple@gmail.com>

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -16,12 +16,11 @@ use crate::proto::MetricFamily;
 /// An interface for encoding metric families into an underlying wire protocol.
 pub trait Encoder {
     /// `encode` converts a slice of MetricFamily proto messages into target
-    /// format and writes the resulting lines to `writer`. It returns the number
-    /// of bytes written and any error encountered. This function does not
-    /// perform checks on the content of the metric and label names,
-    /// i.e. invalid metric or label names will result in invalid text format
+    /// format and writes the resulting lines to `writer`. This function does not
+    /// perform checks on the content of the metrics and label names,
+    /// i.e. invalid metrics or label names will result in invalid text format
     /// output.
-    fn encode<W: Write>(&self, _: &[MetricFamily], _: &mut W) -> Result<()>;
+    fn encode<W: Write>(&self, mfs: &[MetricFamily], writer: &mut W) -> Result<()>;
 
     /// `format_type` returns target format.
     fn format_type(&self) -> &str;
